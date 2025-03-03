1d63cca83042eb1f1025dd9daeb9dc9f648155dd
antoniomari <antonio.mari@epfl.ch>
2025-03-03 16:43:37 +0100

    Commented SAE implementation.

--- SAE/sae.py
+++ SAE/sae.py
@@ -24,6 +24,16 @@ class SparseAutoencoder(nn.Module):
         auxk: int | None,
         dead_steps_threshold: int,
     ):
+        """Sparse Autoencoder constructor
+
+        Args:
+            n_dirs_local (int): # hidden dim of SAE
+            d_model (int): # dim of model activation (input and output dim of SAE)
+            k (int): #TODO: check
+            auxk (int | None): #TODO: check
+            dead_steps_threshold (int): #TODO: check
+        """
+    
         super().__init__()
         self.n_dirs_local = n_dirs_local
         self.d_model = d_model
@@ -37,10 +47,16 @@ class SparseAutoencoder(nn.Module):
         self.pre_bias = nn.Parameter(torch.zeros(d_model))
         self.latent_bias = nn.Parameter(torch.zeros(n_dirs_local))
 
+        # buffer that stores how many iterations have passed since each latent unit was last activated
+        # if this value is higher than a threshold, then that feature is "dead"? 
         self.stats_last_nonzero: torch.Tensor
-        self.register_buffer("stats_last_nonzero", torch.zeros(n_dirs_local, dtype=torch.long))
+        self.register_buffer("stats_last_nonzero", torch.zeros(n_dirs_local, dtype=torch.long))  #TODO: check
 
-        def auxk_mask_fn(x):
+        def auxk_mask_fn(x: torch.Tensor):
+            """ 
+            Masks non-dead features, keeping active the dead ones.
+            """
+            # dead mask is 1 for each dimension that did not activate for more than threshold, 0 otherwise
             dead_mask = self.stats_last_nonzero > dead_steps_threshold
             x.data *= dead_mask  # inplace to save memory
             return x
@@ -49,10 +65,10 @@ class SparseAutoencoder(nn.Module):
 
         ## initialization
 
-        # "tied" init
+        # "tied" init NOTE: since the cloning, we are not actually tieing the weights, are we?
         self.decoder.weight.data = self.encoder.weight.data.T.clone()
 
-        # store decoder in column major layout for kernel
+        # store decoder in column major layout for kernel: TODO: checkout if this operation works as intended
         self.decoder.weight.data = self.decoder.weight.data.T.contiguous().T
 
         unit_norm_decoder_(self)
@@ -71,10 +87,11 @@ class SparseAutoencoder(nn.Module):
 
         os.makedirs(path, exist_ok=True)
 
+        # save config (hidden_dim, input_dim, k, auxk, dead_steps_thresholds)
         with open(os.path.join(path, PATH_TO_CFG), 'w') as f:
             json.dump(cfg, f)
         
-
+        # save model state dict
         torch.save({
             "state_dict": self.state_dict(),
         }, os.path.join(path, PATH_TO_WEIGHTS))
@@ -105,42 +122,55 @@ class SparseAutoencoder(nn.Module):
     def n_dirs(self):
         return self.n_dirs_local
 
-    def encode(self, x):
-        x = x - self.pre_bias
-        latents_pre_act = self.encoder(x) + self.latent_bias
+    def encode(self, x: torch.Tensor):
+        x = x - self.pre_bias # x.shape = TODO: check (Batch?, d_model)
+        latents_pre_act = self.encoder(x) + self.latent_bias  # shape = (Batch, n_dirs_local)
 
+        # gets the K largest activations (and their indices) for each elem of the batch
+        # vals: (batch, K), inds: (batch, K)
         vals, inds = torch.topk(
             latents_pre_act,
             k=self.k,
             dim=-1
         )   
         
-        latents = torch.zeros_like(latents_pre_act)
-        latents.scatter_(-1, inds, torch.relu(vals))
+        # The latents will be 0 for all non-topk activations, ReLU(x) for the topk.
+        latents = torch.zeros_like(latents_pre_act)  # shape = (batch, hidden_dim)
+        latents.scatter_(-1, inds, torch.relu(vals))  # for (b, k), v in (inds, vals): latents[b, k] = ReLU(v)
+        # This is materializing the sparse tensor, in forward this does not happen (use sparse tensor implementation instead)
 
         return latents
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor):
+
+        # NOTE: duplication of code
         x = x - self.pre_bias
-        latents_pre_act = self.encoder(x) + self.latent_bias
+        latents_pre_act: torch.Tensor = self.encoder(x) + self.latent_bias
         vals, inds = torch.topk(
             latents_pre_act,
             k=self.k,
             dim=-1
         )
 
-        ## set num nonzero stat ##
+        ## set num nonzero stat ## NOTE: these stats could be used for regularization 
+        # and to adapt autoencoder dynamically to avoid dead neurons
         tmp = torch.zeros_like(self.stats_last_nonzero)
+
+        # since tmp is 1d tensor, scatter_add_ expects 1d -> iterates on (i, v) in (inds, vals) incrementing tmp[i] += (v > 1e-3)
+        # so tmp contains the number of activations of each latent feature for the current batch
         tmp.scatter_add_(
             0,
             inds.reshape(-1),
-            (vals > 1e-3).to(tmp.dtype).reshape(-1),
+            (vals > 1e-3).to(tmp.dtype).reshape(-1),  # use threshold 1e-3, everything lower considered 0
         )
+
+        # if feature was activated, the stats_last_nonzero is reset to 0, otherwise the value is kept
         self.stats_last_nonzero *= 1 - tmp.clamp(max=1)
         self.stats_last_nonzero += 1
         ## end stats ##
 
-        ## auxk
+        ## Selects the top-auxK dead features 
+        # NOTE: top-auxK are not used for reconstruction, find out how they are used.
         if self.auxk is not None:  # for auxk
             # IMPORTANT: has to go after stats update!
             # WARN: auxk_mask_fn can mutate latents_pre_act!
@@ -155,20 +185,24 @@ class SparseAutoencoder(nn.Module):
 
         ## end auxk
 
+        # applies ReLU activation function to vals (and possibly auxk_vals)
         vals = torch.relu(vals)
         if auxk_vals is not None:
             auxk_vals = torch.relu(auxk_vals)
 
-
+        # rows is batch size, cols is hidden_dim
         rows, cols = latents_pre_act.size()
+        # [0, 1, Batch -1] => [[0], [1], ..., [B -1]] => (k=3) [[0, 0, 0], [1, 1, 1], ..., [B-1, B-1, B-1]] => [0, 0, 0, 1, 1, 1, ..., B-1, B-1, B-1]
         row_indices = torch.arange(rows).unsqueeze(1).expand(-1, self.k).reshape(-1)
-        vals = vals.reshape(-1)
-        inds = inds.reshape(-1)
+        vals = vals.reshape(-1)  # [val_b=1_k=1, val_b=1_k=2, val_b=1_k=3, ...]
+        inds = inds.reshape(-1)  # [ind_b=1_k=1, ind_b=1_k=2, ind_b=1_k=3, ...]
 
-        indices = torch.stack([row_indices.to(inds.device), inds])
-
+        indices = torch.stack([row_indices.to(inds.device), inds]) # creates couples (position_in_batch, index_of_val)
+        # indices is a collection of 2d-indices that specify where a value is stored
+        # sparse tensor, representing a tensor of shape (batch, hidden_dim) -> 
+        # we specify each element for val in vals: T[b, k] = val (where (b, k) is corresponding element of indices
         sparse_tensor = torch.sparse_coo_tensor(indices, vals, torch.Size([rows, cols]))
-
+        # Sparse matrix multiplication for optimized decoding
         recons = torch.sparse.mm(sparse_tensor, self.decoder.weight.T) + self.pre_bias
 
 
@@ -180,7 +214,9 @@ class SparseAutoencoder(nn.Module):
         }
 
     
-    def decode_sparse(self, inds, vals):
+    def decode_sparse(self, inds: torch.Tensor, vals: torch.Tensor):
+        # NOTE: duplicate code, TODO: reuse it?
+        # inds (Batch, K), vals (Batch, K)
         rows, cols = inds.shape[0], self.n_dirs
         
         row_indices = torch.arange(rows).unsqueeze(1).expand(-1, inds.shape[1]).reshape(-1)
@@ -214,3 +250,4 @@ def unit_norm_decoder_grad_adjustment_(autoencoder) -> None:
     autoencoder.decoder.weight.grad +=\
         torch.einsum("bn,bn->n", autoencoder.decoder.weight.data, autoencoder.decoder.weight.grad) *\
         autoencoder.decoder.weight.data * -1
+        
